3799409425df99f676766ddcbff87ee3163e804b
IncogLad <vincenthe5th@yahoo.com>
2023-01-29 18:16:13 +0800

    yep

--- BaseWindow.h
+++ BaseWindow.h
@@ -1,5 +1,26 @@
 #pragma once
-class BaseWindow
+#include <SFML/Graphics.hpp>
+#include <vector>
+
+class BaseWindow: sf::NonCopyable
 {
+public:
+	BaseWindow();
+	~BaseWindow();
+
+	void Run();
+
+	static const unsigned int WINDOW_HEIGHT = 1080;
+	static const unsigned int WINDOW_WIDTH = 1920;
+
+private:
+
+	sf::RenderWindow main_window;
+
+	void render();
+	void processEvents();
+	void update(sf::Time deltaTime);
+
+
 };
 
